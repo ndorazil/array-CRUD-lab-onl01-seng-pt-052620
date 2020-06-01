8e04f49e7b794e1c2f700813e2c68df90698b40e
Nathan Dorazil <nathand1915@gmail.com>
2020-06-01 00:14:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  out_of_ideas ["cool beans", "donald trump", ]
 end
 
 def retrieve_element_from_index(array, index_number)
