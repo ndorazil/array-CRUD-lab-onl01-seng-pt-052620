eb581598b812d7258df5171c7854368061cf88ca
Nathan Dorazil <nathand1915@gmail.com>
2020-06-01 00:11:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  cool_words = ["nifty", "neato"]
+  cool_words.unshift("cool beans")
 end
 
 def remove_element_from_end_of_array(array)
