0f60b367ff741679ae6657145cb63c3d51b507e3
Nathan Dorazil <nathand1915@gmail.com>
2020-06-01 00:17:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,8 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  disney = ["Bolt", "Cinderella"]
-  disney
+  disney = ["wow", "Bolt", "Cinderella"]
+  disney.
 end
 
 def retrieve_last_element_from_array(array)
