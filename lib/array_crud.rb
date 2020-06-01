8c183eda969b1b50b9c82a5614b881f096c38960
Nathan Dorazil <nathand1915@gmail.com>
2020-06-01 00:19:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  out_of_ideas ["wow", "cool beans", "donald trump"]
+  out_of_ideas = ["wow", "cool beans", "donald trump"]
   out_of_ideas.shift
 end
 
