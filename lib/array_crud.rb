042bb43c37222a3e21001144068435ed89b43362
Nathan Dorazil <nathand1915@gmail.com>
2020-06-01 00:08:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   disney = ["Bolt", "Cinderella"]
-  disney << "Little Mermaid"
+  disney << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
